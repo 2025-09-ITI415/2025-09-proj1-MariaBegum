b919d3c501d0903948dc1d3d6cecb4d9fd2a7d88
MariaBegum <mb2452@scarletmail.rutgers.edu>
2025-10-15 22:26:16 -0400

    bubbleprefab

---
+++ Assets/__Scripts/BubbleSpawner.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class BubbleSpawner : MonoBehaviour
+{
+    public GameObject bubblePrefab;
+    public float spawnRate = 1.5f;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnBubble", 1f, spawnRate);
+    }
+
+    void SpawnBubble()
+    {
+        float randomX = Random.Range(-3f, 3f);
+        Vector3 spawnPos = new Vector3(randomX, transform.position.y, 0);
+        Instantiate(bubblePrefab, spawnPos, Quaternion.identity);
+    }
+}
+
